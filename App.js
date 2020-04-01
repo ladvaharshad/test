fc34700f39bcaa4abe39e1f158fd105f67f52051
ladvaharshad <ladvaharshad17@gmail.com>
2020-04-01 18:03:10 +0530

    final commit

--- App.js
+++ App.js
@@ -1,114 +1,87 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component  } from 'react';
+import { AsyncStorage, StyleSheet,ScrollView, View, Text, Button } from 'react-native';
+import { Table, Row,TableWrapper, Rows } from 'react-native-table-component';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      page: 0,
+      tableHead: ['Title', 'URL', 'created_at', 'author'],
+      tableData: [        
+      ],
+      widthArr: [25,25,25,25]
+    };
+    this.getFetchRequest();
+  }
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  componentDidMount() {
+    // this.interval = setInterval(() => this.setState({ 
+    //   page: page+1
+    // }), 10000);
+    this.getFetchRequest();
+    // this.setState({
+      // tableData: this.getFetchRequest()
+    // });
+  }
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  
+ getFetchRequest = () => {
+  fetch(`https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${this.state.page}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
+  }).then((response) => response.json())
+      .then((responseJson) => {
+        
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+
+  render() {
+    const state = this.state;
+    return (
+      <View style={styles.container}>
+      <ScrollView horizontal={true}>
+        <View>
+          <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+            <Row data={state.tableHead} style={styles.header} textStyle={styles.text}/>
+          </Table>
+          <ScrollView style={styles.dataWrapper}>
+            <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+              {/* {
+                tableData.map((rowData, index) => (
+                  <Row
+                    key={index}
+                    data={rowData}
+                    style={[styles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
+                    textStyle={styles.text}
+                  />
+                ))
+              } */}
+            </Table>
+          </ScrollView>
+        </View>
+      </ScrollView>
+    </View>
+    );
+  }
+
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  header: { height: 50, backgroundColor: '#537791' },
+  text: { textAlign: 'center', fontWeight: '100' },
+  dataWrapper: { marginTop: -1 },
+  row: { height: 40, backgroundColor: '#E7E6E1' }
 });
-
-export default App;
